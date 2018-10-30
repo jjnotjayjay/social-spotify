a3904def89f31d1b5b0890e70a618270f640da0b
jjnotjayjay <jj@alumni.usc.edu>
2018-10-30 15:34:45 -0700

    Songs and their associated artists are rendered in a list to the app; improved visual styling of the list of songs. #7

--- src/songs.js
+++ src/songs.js
@@ -1,5 +1,35 @@
 import React from 'react'
 import requestPromise from 'request-promise'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import Avatar from '@material-ui/core/Avatar'
+import ListItemText from '@material-ui/core/ListItemText'
+import { withStyles } from '@material-ui/core/styles'
+
+const OpaqueList = withStyles({
+  root: {
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    maxWidth: '600px',
+    margin: 'auto'
+  }
+})(List)
+
+const NarrowListItem = withStyles({
+  root: {
+    width: '80%'
+  }
+})(ListItem)
+
+const SongText = withStyles({
+  primary: {
+    lineHeight: '1rem',
+    fontSize: '0.8rem'
+  },
+  secondary: {
+    fontSize: '0.7rem'
+  }
+})(ListItemText)
 
 export default class Songs extends React.Component {
   constructor(props) {
@@ -27,8 +57,20 @@ export default class Songs extends React.Component {
   }
 
   render() {
+    const { songs } = this.state
     return (
-      <p>Songs</p>
+      <OpaqueList>
+        {songs.map((song, index) => {
+          return (
+            <NarrowListItem key={index}>
+              <ListItemAvatar>
+                <Avatar src={song.track.album.images[0].url} />
+              </ListItemAvatar>
+              <SongText primary={song.track.name} secondary={song.track.artists[0].name} />
+            </NarrowListItem>
+          )
+        })}
+      </OpaqueList>
     )
   }
 }
