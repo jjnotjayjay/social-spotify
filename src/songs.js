2324a90fa0cee198a29cf3d854fc95ffb5efb357
jjnotjayjay <jj@alumni.usc.edu>
2018-10-30 14:05:14 -0700

    Playlist ID is sent to Spotify API, which returns a list of songs in the playlist and sets it into the app state. #7

--- src/songs.js
+++ src/songs.js
@@ -1,6 +1,31 @@
 import React from 'react'
+import requestPromise from 'request-promise'
 
 export default class Songs extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      songs: []
+    }
+  }
+
+  componentDidMount() {
+    const songDataRequest = {
+      url: 'https://api.spotify.com/v1/playlists/' +
+        this.props.selectedPlaylist +
+        '/tracks',
+      headers: { Authorization: 'Bearer ' + this.props.accessToken },
+      json: true
+    }
+
+    requestPromise(songDataRequest)
+      .then(res =>
+        this.setState({
+          songs: res.items
+        })
+      )
+  }
+
   render() {
     return (
       <p>Songs</p>
