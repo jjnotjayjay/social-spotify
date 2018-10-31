59e237344f6f0ee51f5c5a603742d85e532bcc7e
jjnotjayjay <jj@alumni.usc.edu>
2018-10-31 10:21:19 -0700

    Icon is placed correctly in the upper left hand corner of the app. #22

--- src/page-header.js
+++ src/page-header.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import UserAvatar from './avatar.js'
 import ArrowBack from '@material-ui/icons/ArrowBack'
+import { withStyles } from '@material-ui/core/styles'
+
+const BackButton = withStyles({
+  root: {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    width: '40px',
+    height: '40px',
+    borderRadius: '50%',
+    margin: '2vh',
+    float: 'left'
+  }
+})(ArrowBack)
 
 export default function PageHeader(props) {
   return (
     <div style={{ overflow: 'hidden' }}>
-      {props.view === 'songs' && <ArrowBack />}
+      {props.view === 'songs' && <BackButton />}
       <UserAvatar userImage={props.userImage} />
     </div>
   )
