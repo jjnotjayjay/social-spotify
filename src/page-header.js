2c3a4e6e1539420b1b056e2e48b33ace3a8ed494
jjnotjayjay <jj@alumni.usc.edu>
2018-11-02 11:03:28 -0700

    Import a share icon from Material UI; float the icon to match the existing buttons. #8

--- src/page-header.js
+++ src/page-header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import UserAvatar from './avatar.js'
 import ArrowBack from '@material-ui/icons/ArrowBack'
+import Share from '@material-ui/icons/Share'
 import { withStyles } from '@material-ui/core/styles'
 
 const BackButton = withStyles({
@@ -14,11 +15,23 @@ const BackButton = withStyles({
   }
 })(ArrowBack)
 
+const ShareButton = withStyles({
+  root: {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    width: '40px',
+    height: '40px',
+    borderRadius: '50%',
+    margin: '2vh',
+    float: 'right'
+  }
+})(Share)
+
 export default function PageHeader(props) {
   return (
     <div style={{ overflow: 'hidden' }}>
       {props.view === 'songs' && <BackButton onClick={props.returnToPlaylists} />}
       <UserAvatar userImage={props.userImage} />
+      {props.view === 'songs' && <ShareButton />}
     </div>
   )
 }
