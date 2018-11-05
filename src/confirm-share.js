7f36dc7b5536aa3f7f0ec9b85986319bedf59dca
jjnotjayjay <jj@alumni.usc.edu>
2018-11-04 20:18:28 -0800

    Improved styling of the modal. #8

--- src/confirm-share.js
+++ src/confirm-share.js
@@ -7,6 +7,7 @@ export default function ConfirmShare(props) {
     <Modal open={true} onClose={props.hideConfirmShare}>
       <div className='centered confirm-share'>
         <p>Are you sure you want to share <b>{props.selectedPlaylistName}</b> with <b>{props.recipientOfShare}</b>?</p>
+        <br />
         <Button href="#" size="large" color="primary" variant="contained">Confirm</Button>
       </div>
     </Modal>
