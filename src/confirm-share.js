d2e7ae894712e377e81d2aca8fb7cad131df0338
jjnotjayjay <jj@alumni.usc.edu>
2018-11-04 20:12:54 -0800

    Added a button to the Modal to 'confirm' the desired share. #8

--- src/confirm-share.js
+++ src/confirm-share.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Modal from '@material-ui/core/Modal'
+import Button from '@material-ui/core/Button'
 
 export default function ConfirmShare(props) {
   return (
     <Modal open={true} onClose={props.hideConfirmShare}>
       <div className='centered confirm-share'>
-        <p>{props.recipientOfShare} {props.selectedPlaylistName}</p>
+        <p>Are you sure you want to share <b>{props.selectedPlaylistName}</b> with <b>{props.recipientOfShare}</b>?</p>
+        <Button href="#" size="large" color="primary" variant="contained">Confirm</Button>
       </div>
     </Modal>
   )
